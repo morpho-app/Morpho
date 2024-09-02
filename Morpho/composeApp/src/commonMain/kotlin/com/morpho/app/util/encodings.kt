79182fde92a42a4263278962ee104049ccc2f517
Orual <orual.nbc@proton.me>
2024-09-01 23:29:48 -0400

    Fixed the offset facets https://github.com/morpho-app/Morpho/issues/31

--- Morpho/composeApp/src/commonMain/kotlin/com/morpho/app/util/encodings.kt
+++ Morpho/composeApp/src/commonMain/kotlin/com/morpho/app/util/encodings.kt
@@ -1,7 +1,10 @@
 package com.morpho.app.util
 
 
+import okio.ByteString
 import okio.ByteString.Companion.encodeUtf8
+import kotlin.experimental.and
+
 /*import org.jetbrains.skia.BreakIterator
 
 
@@ -21,3 +24,20 @@ fun utf8Length(string: String): Int {
     return string.encodeUtf8().size
 }
 
+/**
+ * Determines if this byte is a single-byte utf-8 character.
+ */
+fun Byte.isSingleByte(): Boolean {
+    return (this and 0b10000000.toByte()) == 0.toByte()
+}
+
+fun utf16FacetIndex(text: String, utf8: ByteString, start: Int, end: Int): Pair<Int, Int> {
+    val utf8FacetText = utf8.substring(start, end)
+    //println("utf8FacetText: '${utf8FacetText.utf8()}'")
+    //println("utf8Start: ${utf8.indexOf(utf8FacetText)}, utf8End: ${utf8.indexOf(utf8FacetText) + utf8FacetText.size}")
+    val utf16FacetText = utf8FacetText.utf8()
+    val utf16Start = text.indexOf(utf16FacetText)
+    val utf16End = utf16Start + utf16FacetText.length
+    //println("utf16Start: $utf16Start, utf16End: $utf16End")
+    return Pair(utf16Start, utf16End)
+}
